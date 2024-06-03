ea53d9f7258490222c549cc0fd40e3ea0178d91c
xeroxis-xs <txsheng0609@hotmail.com>
2024-06-03 18:07:29 +0800

    update requirements.txt and settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -80,6 +80,7 @@ SIMPLE_JWT = {
     'USER_ID_CLAIM': 'user_id',
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
+    "SIGNING_KEY": SECRET_KEY,
 }
 
 AUTHENTICATION_BACKENDS = [
