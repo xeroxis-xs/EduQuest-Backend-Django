beb9958c94d37d0d27a2430d69b56af928b50dff
Teoh Xi Sheng <txsheng0609@hotmail.com>
2024-08-19 00:02:18 +0800

    added exception for open-question type and poll type

--- app/api/excel.py
+++ app/api/excel.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import json
 
 
 class Excel():
@@ -12,6 +11,7 @@ class Excel():
         self.question_list = []
         self.user_list = []
         self.question_list = []
+        self.question_type_mapping_list = []
 
     def read_excel_sheets(self, excel_file):
         self.xls = pd.ExcelFile(excel_file, engine='openpyxl')
@@ -19,29 +19,50 @@ class Excel():
         self.main_results_sheet = pd.read_excel(self.xls, sheet_name=self.xls.sheet_names[0])
 
     def get_questions(self):
-        for i in range(len(self.sheets)):
-            if self.sheets[i] != 'Main results' and self.sheets[i] != 'Wall':
-                question = dict()
-                df = pd.read_excel(self.xls, sheet_name=self.xls.sheet_names[i])
-                question['text'] = df.head(0).columns[0]
-                question['number'] = i
-                question['answers'] = []
+        # print("Getting Questions")
 
+        for sheet_name in self.sheets:
+            # Skip 'Main results' and 'Wall' sheets
+            if sheet_name in ['Main results', 'Wall']:
+                continue
+
+            df = pd.read_excel(self.xls, sheet_name=sheet_name)
+            num_rows = df.shape[0]
+            # print(f"Sheet: {sheet_name}, Number of rows: {num_rows}")
+
+            question = {
+                'text': df.columns[0],
+                'number': self.sheets.index(sheet_name),
+                'answers': []
+            }
+            is_mcq = False
+
+            # Check if the question is a MCQ by looking for 'Maximum score' row
+            for k in range(num_rows):
+                if df.iloc[k, 0] == 'Maximum score':
+                    question['max_score'] = df.iloc[k, 1]
+                    self.question_type_mapping_list.append({'sheet_name': sheet_name, 'is_mcq': True})
+                    is_mcq = True
+                    break
+
+            # Get the answer options if it is a MCQ
+            if is_mcq:
                 j = 2
-                while not pd.isnull(df.iloc[j, 0]):
-                    answer = dict()
-                    answer['text'] = df.iloc[j, 0]
-                    answer['is_correct'] = False
-                    question['answers'].append(answer)
+                while j < num_rows and not pd.isnull(df.iloc[j, 0]):
+                    question['answers'].append({
+                        'text': df.iloc[j, 0],
+                        'is_correct': False
+                    })
                     j += 1
-
-                k = 0
-                while df.iloc[k, 0] != 'Maximum score':
-                    k += 1
-                question['max_score'] = df.iloc[k, 1]
-
                 self.question_list.append(question)
+                # print(f"Max score: {question['max_score']}, Number of answer options: {len(question['answers'])}\n")
+            else:
 
+                self.question_type_mapping_list.append({
+                    'sheet_name': sheet_name,
+                    'is_mcq': False
+                })
+                # print("Open Question or Poll Type, Skipping\n")
         return self.question_list
 
     def get_users(self):
@@ -57,24 +78,28 @@ class Excel():
     def get_user_question_attempts(self, email):
         user_question_attempt_list = []
         i = 0
-        # print(f"Email: {email}")
-        # Scan each row
+        # Scan each row in main result sheet
         while not pd.isnull(self.main_results_sheet.iloc[i, 0]):
             # If the email matches
             if self.main_results_sheet.iloc[i, 4].upper() == email.upper():
-                # Iterate through each question
-                for j in range(5, len(self.main_results_sheet.columns)-1):
+                # Create a list of indices for columns that are MCQ
+                mcq_indices = [i for i in range(5, len(self.main_results_sheet.columns) - 1) if
+                               self.question_type_mapping_list[i - 5]['is_mcq']]
+                # Iterate through each mcq question to the end of the columns
+                for j in mcq_indices:
                     user_question_attempt = dict()
                     wooclap_selected_answer = self.main_results_sheet.iloc[i, j]
                     # Split the wooclap_selected_answer to get the answers part
                     _, wooclap_selected = wooclap_selected_answer.split(" - ", 1)
-                    user_question_attempt['question'] = self.question_list[j-5]['text']
+                    user_question_attempt['question'] = self.question_list[j - 5]['text']
                     user_question_attempt['selected_answers'] = []
                     # Iterate through each answer string
-                    for answer in self.question_list[j-5]['answers']:
+                    for answer in self.question_list[j - 5]['answers']:
                         if answer['text'] in wooclap_selected:
                             user_question_attempt['selected_answers'].append(answer['text'])
                     user_question_attempt_list.append(user_question_attempt)
             i += 1
-        # print(f"User question attempt list: {user_question_attempt_list}")
+        # # Export to JSON
+        # with open('questions.json', 'w') as f:
+        #     json.dump(user_question_attempt_list, f)
         return user_question_attempt_list
--- app/api/management/commands/test_import_excel.py
+++ app/api/management/commands/test_import_excel.py
@@ -11,99 +11,107 @@ class Command(BaseCommand):
         self.main_results_sheet = None
         self.question_list = []
         self.user_list = []
-        self.user_quest_attempt_list = []
-        self.user_question_attempt_list = []
+        self.question_list = []
+        self.question_type_mapping_list = []
 
     def handle(self, *args, **kwargs):
         self.read_excel_sheets()
         self.get_questions()
         self.get_users()
-        # self.get_user_quest_attempts()
-        self.get_user_question_attempts("testuser@e.ntu.edu.sg")
+        self.get_user_question_attempts("c210101@e.ntu.edu.sg")
 
     def read_excel_sheets(self):
-        excel_file = 'app/api/management/commands/JBRYGU_Introduction_to_Database_-_Quiz_1-results.xlsx'
+        print("Reading Excel Sheets")
+        excel_file = 'app/api/management/commands/HPSPXY_Introduction_to_Database_-_Quiz_1_Open_Question__Poll-results.xlsx'
         self.xls = pd.ExcelFile(excel_file, engine='openpyxl')
         self.sheets = self.xls.sheet_names
         self.main_results_sheet = pd.read_excel(self.xls, sheet_name=self.xls.sheet_names[0])
 
     def get_questions(self):
-        for i in range(len(self.sheets)):
-            if self.sheets[i] != 'Main results' and self.sheets[i] != 'Wall':
-                # print(f"Sheet name: {self.sheets[i]}")
-                question = dict()
-                df = pd.read_excel(self.xls, sheet_name=self.xls.sheet_names[i])
-                question['text'] = df.head(0).columns[0].strip()
-                question['number'] = i
-                # print(f"Question text: {question['text']}")
-                question['answers'] = []
+        print("Getting Questions")
 
+        for sheet_name in self.sheets:
+            # Skip 'Main results' and 'Wall' sheets
+            if sheet_name in ['Main results', 'Wall']:
+                continue
+
+            df = pd.read_excel(self.xls, sheet_name=sheet_name)
+            num_rows = df.shape[0]
+            print(f"Sheet: {sheet_name}, Number of rows: {num_rows}")
+
+            question = {
+                'text': df.columns[0],
+                'number': self.sheets.index(sheet_name),
+                'answers': []
+            }
+            is_mcq = False
+
+            # Check if the question is a MCQ by looking for 'Maximum score' row
+            for k in range(num_rows):
+                if df.iloc[k, 0] == 'Maximum score':
+                    question['max_score'] = df.iloc[k, 1]
+                    self.question_type_mapping_list.append({'sheet_name': sheet_name, 'is_mcq': True})
+                    is_mcq = True
+                    break
+
+            # Get the answer options if it is a MCQ
+            if is_mcq:
                 j = 2
-                while not pd.isnull(df.iloc[j, 0]):
-                    answer = dict()
-                    answer['text'] = df.iloc[j, 0]
-                    answer['is_correct'] = False
-                    # print(f"Answer: {df.iloc[j, 0]}")
-                    question['answers'].append(answer)
+                while j < num_rows and not pd.isnull(df.iloc[j, 0]):
+                    question['answers'].append({
+                        'text': df.iloc[j, 0],
+                        'is_correct': False
+                    })
                     j += 1
-
-                k = 0
-                while df.iloc[k, 0] != 'Maximum score':
-                    k += 1
-                question['max_score'] = df.iloc[k, 1]
-                # print(f"Max Score: {df.iloc[k, 1]}")
-
                 self.question_list.append(question)
+                print(f"Max score: {question['max_score']}, Number of answer options: {len(question['answers'])}\n")
+            else:
 
-        # print(f"Question list: {self.question_list}")
+                self.question_type_mapping_list.append({
+                    'sheet_name': sheet_name,
+                    'is_mcq': False
+                })
+                print("Open Question or Poll Type, Skipping\n")
+        return self.question_list
 
     def get_users(self):
+        print("Getting Users")
         i = 0
         while not pd.isnull(self.main_results_sheet.iloc[i, 0]):
             user = dict()
-            user['email'] = self.main_results_sheet.iloc[i, 4]
+            user['email'] = self.main_results_sheet.iloc[i, 4].upper()
             user['username'] = self.main_results_sheet.iloc[i, 1]
-            self.user_list.append(user)
             i += 1
+            self.user_list.append(user)
         return self.user_list
-
-    def get_user_quest_attempts(self):
-        user_quest_attempt = dict()
-        i = 0
-        while not pd.isnull(self.main_results_sheet.iloc[i, 0]):
-            # Email
-            user_quest_attempt['user'] = self.main_results_sheet.iloc[i, 4]
-            user_quest_attempt['graded'] = False
-            user_quest_attempt['time_taken'] = 0
-            user_quest_attempt['quest'] = None
-            self.user_quest_attempt_list.append(user_quest_attempt)
 
     def get_user_question_attempts(self, email):
         user_question_attempt_list = []
         i = 0
-        print(f"Email: {email}")
-        # Scan each row
+        # Scan each row in main result sheet
         while not pd.isnull(self.main_results_sheet.iloc[i, 0]):
             # If the email matches
             if self.main_results_sheet.iloc[i, 4].upper() == email.upper():
-                # Iterate through each question
-                print(f"Email: {self.main_results_sheet.iloc[i, 4]}")
-
-                for j in range(5, len(self.main_results_sheet.columns)-1):
+                # Create a list of indices for columns that are MCQ
+                mcq_indices = [i for i in range(5, len(self.main_results_sheet.columns) - 1) if
+                               self.question_type_mapping_list[i - 5]['is_mcq']]
+                # Iterate through each mcq question to the end of the columns
+                for j in mcq_indices:
                     user_question_attempt = dict()
                     wooclap_selected_answer = self.main_results_sheet.iloc[i, j]
                     # Split the wooclap_selected_answer to get the answers part
                     _, wooclap_selected = wooclap_selected_answer.split(" - ", 1)
-
-                    user_question_attempt['question'] = self.question_list[j-5]['text']
+                    user_question_attempt['question'] = self.question_list[j - 5]['text']
                     user_question_attempt['selected_answers'] = []
                     # Iterate through each answer string
-                    for answer in self.question_list[j-5]['answers']:
+                    for answer in self.question_list[j - 5]['answers']:
                         if answer['text'] in wooclap_selected:
                             user_question_attempt['selected_answers'].append(answer['text'])
                     user_question_attempt_list.append(user_question_attempt)
             i += 1
-        print(f"Selected answers: {user_question_attempt_list}")
+        # Export to JSON
+        with open('questions.json', 'w') as f:
+            json.dump(user_question_attempt_list, f)
         return user_question_attempt_list
 
 
