5b82c87525ecfcbf4f7ae735bbf4f54f0b451c99
Teoh Xi Sheng <txsheng0609@hotmail.com>
2024-09-23 02:25:42 +0800

    remove exception handler

--- app/core/settings.py
+++ app/core/settings.py
@@ -64,8 +64,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    ),
-    'EXCEPTION_HANDLER': 'api.exceptions.CustomExceptionHandler',
+    )
 }
 
 AZURE_CLIENT_ID = os.environ.get('AZURE_AD_CLIENT_ID')
